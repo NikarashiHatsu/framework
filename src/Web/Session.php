95d21957d705ed9650e289af2a5d9e69a147f744
NikarashiHatsu <yourlovelydev@gmail.com>
2020-09-07 16:31:14 +0700

    Added \Session and changed on how  SessionTrait works

--- src/Web/Session.php
+++ src/Web/Session.php
@@ -4,13 +4,6 @@ namespace Shiroyuki\Web;
 
 trait Session {
   /**
-   * "All" session storage.
-   * 
-   * @var array $sessions
-   */
-  public $sessions = [];
-
-  /**
    * Set the sessions.
    * 
    * @return void
@@ -35,7 +28,12 @@ trait Session {
    */
   private function boot()
   {
-    $this->sessions = $_SESSION;
+    echo json_encode($_SESSION);
+    // if(count(self::$flashed) > 0) {
+    //   echo "Yes";
+    // } else {
+    //   echo "No";
+    // }
   }
 
   /**
@@ -44,13 +42,9 @@ trait Session {
    * @param string $requested_data
    * @return bool
    */
-  public function has($requested_data)
+  public static function has($requested_data)
   {
-    if(isset($this->session[$requested_data])) {
-      return true;
-    }
-    
-    return false;
+    return (isset($_SESSION[$requested_data])) ? true : false;
   }
 
   /**
@@ -59,9 +53,24 @@ trait Session {
    * @param string $requested_data
    * @return string
    */
-  public function get($requested_data)
+  public static function get($requested_data)
   {
-    return $this->sessions[$requested_data];
+    return $_SESSION[$requested_data];
+  }
+
+  /**
+   * Show the data, then delete the data right away after it's shown.
+   * 
+   * @param string $requested_data
+   * @return string
+   */
+  public static function flash($requested_data)
+  {
+    $session = $_SESSION[$requested_data];
+
+    unset($_SESSION[$requested_data]);
+
+    return $session;
   }
 
   /**
@@ -79,7 +88,7 @@ trait Session {
       $key = $parsed[0];
       $value = $parsed[1];
 
-      $this->sessions[$key] = $value;
+      $_SESSION[$key] = $value;
     } else {
       
       // If the arguments passed are 'array'
@@ -87,7 +96,7 @@ trait Session {
         $array = $parsed[0];
   
         foreach($array as $key => $value) {
-          $this->sessions[$key] = $value;
+          $_SESSION[$key] = $value;
         }
       }
     }
@@ -100,7 +109,7 @@ trait Session {
    */
   public function build()
   {
-    foreach($this->sessions as $key => $item) {
+    foreach($_SESSION as $key => $item) {
       $_SESSION[$key] = $item;
     }
 
